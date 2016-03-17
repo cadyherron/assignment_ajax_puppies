e3889a0aaa15e411f18077112909e192064a8ae0
Deepa Kackar <dkackar99@yahoo.com>
2016-03-17 14:36:12 -0700

    Added initial Ajax stuff

---
+++ puppies.js
@@ -0,0 +1,166 @@
+
+// $.ajax( {
+
+//   url: "post",
+
+//   data: {
+//       id: 123
+//   },
+
+//   type: "GET",
+
+//   // the type of data we expect back
+//   dataType : "json",
+
+//   success: function( json ) {
+
+//       // for example, build a post object onto the body
+//       $( "<h1/>" ).text( json.title ).appendTo( "body" );
+//       $( "<div class=\"content\"/>").html( json.html ).appendTo( "body" );
+//   },
+
+  
+//   error: function( xhr, status, errorThrown ) {
+//       alert( "Sorry, there was a problem!" );
+//       console.log( "Error: " + errorThrown );
+//       console.log( "Status: " + status );
+//       console.dir( xhr );
+//   },
+
+//   // Complete callback to run regardless of the outcome
+//   complete: function( xhr, status ) {
+//       alert( "The request is complete!" );
+//   }
+// });
+
+
+// $.get -- GET requests
+// $.post -- POST requests
+// $.getScript -- retrieve a script
+// $.getJSON -- GET request expecting JSON in return
+
+// $.get( url [, data] [, success callback] [, data type] )
+
+// $( "#result" ).load( "ajax/test.html" );
+
+// // With a "complete" callback that fires for each
+// // element the AJAX response was inserted into
+// $( "#result" ).load( "ajax/test.html", function() {
+//   alert( "Load was performed." );
+// });
+
+
+// ajaxStart fires when the very first AJAX request begins (so not for overlapping requests)
+// ajaxStop fires when there are no more AJAX requests running
+// ajaxSuccess fires whenever any AJAX request succeeds
+// ajaxError fires whenever any AJAX request fails
+// ajaxComplete fires whenever any AJAX request complete
+
+
+
+
+//jQuery already loaded elsewhere
+
+
+//here's the revealing module
+var Puppies = ( function(){
+    // this private variable is the address you always hit up
+    var _puppiesPath = "https://ajax-puppies.herokuapp.com/puppies.json";
+    var _breedPath = "https://ajax-puppies.herokuapp.com/breeds.json";
+    // takes a callback function
+
+
+    function timeSince(date) {
+
+        var seconds = Math.floor((new Date() - date) / 1000);
+
+        var interval = Math.floor(seconds / 31536000);
+
+        if (interval > 1) {
+            return interval + " years";
+        }
+        interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+            return interval + " months";
+        }
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+            return interval + " days";
+        }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+            return interval + " hours";
+        }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+            return interval + " minutes";
+        }
+        return Math.floor(seconds) + " seconds";
+    }
+
+
+    function showBreeds(breeds) {
+
+      for (var i = 0; i < breeds.length; i++) {
+        $("select").append('<option value="' + breeds[i].id + '">' + 
+          breeds[i].name + '</option>');
+      }
+    
+    }
+
+    function showPuppies(puppies) {
+      for (var i = 0; i < puppies.length; i++) {
+        $(".puppies-list").append('<li>' + puppies[i].name + ' (' + puppies[i].breed.name + '),  created' + puppies[i].created_at +
+          ' ago </li>');
+      }    
+    }
+
+    function breedList(){
+
+      $.ajax( {
+         url: _breedPath,
+         type: 'GET',
+         dataType: 'json',
+         success: function(json) { 
+          showBreeds(json); 
+         },
+         error: function() { 
+           console.log("Got an error"); 
+         },
+         complete: function() { 
+           console.log("Done!"); 
+         }
+     });
+    }
+
+    function puppiesList() {
+
+      $.ajax( {
+         url: _puppiesPath,
+         type: 'GET',
+         dataType: 'json',
+         success: function(json) { 
+            showPuppies(json); 
+         },
+         error: function() { 
+           console.log("Got an error"); 
+         },
+         complete: function() { 
+           console.log("Puppies List Done!"); 
+         }
+      });
+
+    };
+
+
+    return {
+      puppies: puppiesList,
+      breeds: breedList,
+    };
+
+})();
+
+$(document).ready(function( ){
+    Puppies.puppies();
+    Puppies.breeds();
+});
